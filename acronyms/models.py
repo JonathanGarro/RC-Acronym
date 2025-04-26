c8c63d722ba18a813cf7d334d290f9425be1ef62
jonathangarro <jonathan.garro@gmail.com>
2025-04-26 12:19:24 -0700

    update models

--- acronyms/models.py
+++ acronyms/models.py
@@ -2,14 +2,16 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
 
+
 class Category(models.Model):
     name = models.CharField(max_length=100)
-    
+
     def __str__(self):
         return self.name
-    
+
     class Meta:
         verbose_name_plural = "Categories"
+
 
 class Acronym(models.Model):
     acronym = models.CharField(max_length=50)
@@ -19,13 +21,13 @@ class Acronym(models.Model):
     created_by = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    
+
     def __str__(self):
         return f"{self.acronym} - {self.definition}"
-    
+
     def get_absolute_url(self):
         return reverse('acronym-detail', kwargs={'pk': self.pk})
-    
+
     class Meta:
         ordering = ['acronym']
         verbose_name_plural = "Acronyms"
