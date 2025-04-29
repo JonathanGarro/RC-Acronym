3dd48d5372524e5a257d70c2a6f1d1d63667f262
jonathangarro <jonathan.garro@gmail.com>
2025-04-29 11:15:59 -0700

    adjust import formatting

--- acronym_project/asgi.py
+++ acronym_project/asgi.py
@@ -3,7 +3,6 @@ ASGI config for acronym_project project.
 """
 
 import os
-
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'acronym_project.settings')
