822e1364c0d525d5ef8c1315928ea6b042d9a736
jonathangarro <jonathan.garro@gmail.com>
2025-05-18 11:28:04 -0700

    include NS on admin

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,19 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
+from .models import Profile, NationalSociety
 
-# Additional user admin customization can be added here
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'Profile'
+
+class UserAdmin(BaseUserAdmin):
+    inlines = (ProfileInline,)
+
+# Register NationalSociety model
+admin.site.register(NationalSociety)
+
+# Re-register UserAdmin
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
