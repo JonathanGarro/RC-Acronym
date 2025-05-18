9a63673503422de44c1f5446c074e08b382c9b36
jonathangarro <jonathan.garro@gmail.com>
2025-05-18 11:27:38 -0700

    update forms with additional user info

--- users/forms.py
+++ users/forms.py
@@ -1,17 +1,28 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile, NationalSociety
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
-    
+    first_name = forms.CharField(max_length=30)
+    last_name = forms.CharField(max_length=30)
+
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
 
 class UserUpdateForm(forms.ModelForm):
     email = forms.EmailField()
-    
+    first_name = forms.CharField(max_length=30)
+    last_name = forms.CharField(max_length=30)
+
     class Meta:
         model = User
-        fields = ['username', 'email']
+        fields = ['username', 'first_name', 'last_name', 'email']
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['bio', 'department', 'job_title', 'phone_number', 'profile_picture', 'national_society']
